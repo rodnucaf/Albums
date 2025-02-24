8ecb2e38de5c1180898ab5b7b8e790aa113afed5
unknown <facundoxrabago@gmail.com>
2025-02-24 19:28:00 -0300

    agregado servicio DbContext

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Albums.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AlbumContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
