904aad0ba0fcf49111c89b8df88ecd55ff5335e5
unknown <facundoxrabago@gmail.com>
2025-02-24 19:02:59 -0300

    Creado Modelo

---
+++ Models/Album.cs
@@ -0,0 +1,9 @@
+﻿namespace Albums.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
