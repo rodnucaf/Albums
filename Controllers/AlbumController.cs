80e57773f0ab565dd357fb5785f8a1bfef0abfff
unknown <facundoxrabago@gmail.com>
2025-02-26 19:09:57 -0300

    Creado controlador

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Albums.Controllers
+{
+    public class AlbumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
