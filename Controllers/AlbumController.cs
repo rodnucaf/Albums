53857eb89b5091a75e11d12a0d44b3bef7eaf039
unknown <facundoxrabago@gmail.com>
2025-02-26 19:24:30 -0300

    creado metodo index

--- Controllers/AlbumController.cs
+++ Controllers/AlbumController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Albums.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Albums.Controllers
 {
     public class AlbumController : Controller
     {
-        public IActionResult Index()
+        private readonly AlbumContext _context;
+
+        public AlbumController(AlbumContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var album = await _context.Albums.ToListAsync();
+
+
+            return View(album);
         }
     }
 }
