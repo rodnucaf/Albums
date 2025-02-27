1f1580f284fba9e24521cd2266fbed874bc91266
unknown <facundoxrabago@gmail.com>
2025-02-27 18:36:52 -0300

    agregada acción Create

--- Controllers/AlbumsController.cs
+++ Controllers/AlbumsController.cs
@@ -1,4 +1,5 @@
 ﻿using Albums.Data;
+using Albums.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,5 +21,24 @@ namespace Albums.Controllers
 
             return View(album);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Date")] Album album) 
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Albums.Add(album);
+                await _context.SaveChangesAsync();
+                return View(RedirectToAction("index"));
+            }
+            return View(album);
+        }
+
+
     }
 }
