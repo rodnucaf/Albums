32b47e40a5064ea2fea5240bc208f0a6aaa4ef7f
unknown <facundoxrabago@gmail.com>
2025-03-05 18:07:26 -0300

    sobreescrito OnModelCreating, agregada data para migración

--- Data/AlbumContext.cs
+++ Data/AlbumContext.cs
@@ -5,7 +5,16 @@ namespace Albums.Data
 {
     public class AlbumContext :DbContext
     {
-        public AlbumContext(DbContextOptions<AlbumContext>options) : base(options){ }   
+        public AlbumContext(DbContextOptions<AlbumContext>options) : base(options){ }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre {Id = 1, Name="Rock"},
+                new Genre {Id = 2, Name="Shoegaze"},
+                new Genre {Id=3, Name="Dreampop"}                
+            );
+        }
 
         public DbSet<Album> Albums { get; set; }
         
