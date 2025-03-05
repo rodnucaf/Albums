f85dae27455c3afb492cdb0d9b80a8e7d1923d57
unknown <facundoxrabago@gmail.com>
2025-03-05 19:26:32 -0300

    agregada migracion, columna Author

---
+++ Migrations/20250305220842_columna author.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Albums.Migrations
+{
+    /// <inheritdoc />
+    public partial class columnaauthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GenreName",
+                table: "Albums");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Albums");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GenreName",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
