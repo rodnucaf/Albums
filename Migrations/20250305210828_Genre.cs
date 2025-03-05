fed01abcdc403d41f5937b09845c6c1a10cf38ca
unknown <facundoxrabago@gmail.com>
2025-03-05 18:17:09 -0300

    agregada migracion

---
+++ Migrations/20250305210828_Genre.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Albums.Migrations
+{
+    /// <inheritdoc />
+    public partial class Genre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Albums",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GenreName",
+                table: "Albums",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Rock" },
+                    { 2, "Shoegaze" },
+                    { 3, "Dreampop" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_GenreId",
+                table: "Albums",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Albums_Genres_GenreId",
+                table: "Albums",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Albums_Genres_GenreId",
+                table: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Albums_GenreId",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "GenreName",
+                table: "Albums");
+        }
+    }
+}
