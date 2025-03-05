983ecc044900691081f6ba8cbba56faa86f6bf5e
unknown <facundoxrabago@gmail.com>
2025-03-05 17:55:20 -0300

    agregados atributos

--- Models/Album.cs
+++ Models/Album.cs
@@ -1,9 +1,16 @@
-﻿namespace Albums.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Albums.Models
 {
     public class Album
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Date { get; set; }
+
+        public int GenreId { get; set; }
+
+        [ForeignKey("GenreId")]
+        public string? GenreName { get; set; }
     }
 }
